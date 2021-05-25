33b04e3ae38bef44f2d7093fb595ab72f4ae5ad6
hdkim <eastshine94@naver.com>
2021-05-25 16:59:53 +0900

    hide .env

---
+++ ormconfig.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity.js'],
+  synchronize: true
+};
